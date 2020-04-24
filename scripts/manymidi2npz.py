a0829953907dcd6b5bc04ad0aafa1eff63d82752
David Schlachter <david+gh@schlachter.ca>
2020-04-24 03:50:52 +0000

    Add script to convert many MIDI to one npz

---
+++ scripts/manymidi2npz.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# modified from https://github.com/salu133445/musegan/issues/51
+
+
+from pypianoroll import Multitrack
+import numpy as np
+import os
+
+# Parameters
+SRC_DIR="/home/azure/musegan/data/classical-2_0-0"
+FILENAMES = [] # The input MIDI filenames
+RESULT_FILENAME= './classical-2_0-0.npz' # The resulting filename
+N_TRACKS = 2
+BEAT_RESOLUTION = 12 # The beat resolution
+
+# Initialize an empty list to collect the results
+results = []
+
+# Find all the midi files
+for root, dirs, files in os.walk(SRC_DIR):
+    for file in files:
+        if file.endswith(".mid"):
+            FILENAMES.append(os.path.join(root, file))
+
+
+# Iterate through all the MIDI files
+for filename in FILENAMES:
+    
+    # Parse the MIDI file into multitrack pianoroll
+    try:
+        multitrack  = Multitrack(filename, beat_resolution=BEAT_RESOLUTION)
+    except:
+        continue
+
+    # Pad to multtple
+    multitrack.pad_to_multiple(4 * BEAT_RESOLUTION)
+
+    # Binarize the pianoroll
+    multitrack.binarize()
+
+    # Sort the tracks according to program number
+    multitrack.tracks.sort(key=lambda x: x.program)
+
+    # Bring the drum track to the first track
+    multitrack.tracks.sort(key=lambda x: ~x.is_drum)
+
+    # Get the stacked pianoroll
+    pianoroll = multitrack.get_stacked_pianorolls()
+
+    # Check length
+    if pianoroll.shape[0] < 4 * 4 * BEAT_RESOLUTION:
+        continue
+
+    # Keep only the mid-range pitches
+    pianoroll = pianoroll[:, 24:108]
+
+    # Reshape and get the phrase pianorolls
+    pianoroll = pianoroll.reshape(-1, 4 * BEAT_RESOLUTION, 84, N_TRACKS)
+    results.append(np.concatenate(
+        [pianoroll[:-3], pianoroll[1:-2], pianoroll[2:-1], pianoroll[3:]], 1))
+
+result = np.concatenate(results, 0)
+# NOTE: You might want to shuffle the training data here
+np.savez_compressed(
+    RESULT_FILENAME, nonzero=np.array(result.nonzero()),
+    shape=result.shape)
